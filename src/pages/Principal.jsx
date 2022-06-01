88766cfc9058b258c862dabce9b39f4c68c50c02
Caio Silva <Caio.Souza@Honeywell.com>
2022-06-01 09:56:35 -0300

    lista de pessoas e grupos quase pronta

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -2,7 +2,19 @@ import "../styles/principal.css";
 import "../styles/main.css";
 import { Link } from "react-router-dom";
 
+import IconPessoa from "../assets/icon-person.png";
+import { useEffect, useState } from "react";
+
 function Principal() {
+
+  const [listaPessoas, setListaPessoas] = useState([]);
+
+  useEffect(() => {
+    setListaPessoas([]);
+    let listaAux = [{ nome: "Caio Zetula" }, { nome: "Luis Soares" }, { nome: "Fernando" }];
+    setListaPessoas(listaAux);
+  }, [])
+
   return (
     <div className="container-principal">
       <div className="menu">
@@ -12,19 +24,35 @@ function Principal() {
         </Link>
       </div>
       <div className="containers">
+        {/* TELA PESSOAS */}
         <div className="container-geral container-pessoas">
           <div className="titulo-container">
-              <h2 className="texto-titulo-container">Pessoas</h2>
+            <h2 className="texto-titulo-container">Pessoas & Grupos</h2>
+            <div className="container-lista-pessoas">
+              {listaPessoas.length > 0 ?
+                listaPessoas.map(pessoa => {
+                  return (
+                    <div className="item-lista-pessoas">
+                      <img className="icon-pessoa" src={IconPessoa} alt="Minha Figura" />
+                      <h3 className="texto-nome-pessoa">{pessoa.nome}</h3>
+                    </div>
+                  );
+                })
+                :
+                <h3 className="texto-vazio">Lista Vazia</h3>
+              }
+            </div>
           </div>
         </div>
+
         <div className="container-geral container-chat">
-        <div className="titulo-container">
-              <h2 className="texto-titulo-container">Chat</h2>
+          <div className="titulo-container">
+            <h2 className="texto-titulo-container">Chat</h2>
           </div>
         </div>
         <div className="container-geral container-posts">
-        <div className="titulo-container">
-              <h2 className="texto-titulo-container">Posts</h2>
+          <div className="titulo-container">
+            <h2 className="texto-titulo-container">Posts</h2>
           </div>
         </div>
       </div>
